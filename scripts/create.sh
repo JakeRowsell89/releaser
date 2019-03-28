24cc1736273940a1a67602d63957d83d04ffbaae
Deepika Dixit <deepika.dixit@bglgroup.co.uk>
2019-03-28 11:48:37 +0000

    Parameterise credentials

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,6 +1,6 @@
 curl \
    -D- \
-   -u jake.rowsell@bglgroup.co.uk:hNND6npUih1G7h0AMexV9DF9 \
+   -u $JIRA_USER:$JIRA_TOKEN \
    -X POST \
    --data "{\"fields\":{\"project\":{\"key\":\"LEGO\"},\"summary\":\"REST ye merry gentlemen.\",\"description\":\"Creating of an issue using project keys and issue type names using the REST API\",\"issuetype\":{\"name\":\"Bug\"}}}" \
    -H "Content-Type: application/json" \
