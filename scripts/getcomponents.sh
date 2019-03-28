fef333ab36b86b0793724780433a8dfb994ab72d
Jake Rowsell <jakerowsell@bgl-mac-009556.bgl.financial.local>
2019-03-28 12:50:52 +0000

    Get just data

--- scripts/getcomponents.sh
+++ scripts/getcomponents.sh
@@ -1,7 +1,5 @@
 curl \
-   -D- \
-   -i \
    -u $JIRA_USER:$JIRA_TOKEN \
-   -X GET \
+   -G \
    -H "Content-Type: application/json" \
-   https://comparethemarket.atlassian.net/rest/api/latest/issue/LEGO-400 > output.json
+   https://comparethemarket.atlassian.net/rest/api/latest/issue/LEGO-400 | jq .fields.components
