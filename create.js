516e588f269197526ee8c4a8fd709057f70f62cf
Deepika Dixit <deepika.dixit@bglgroup.co.uk>
2019-03-29 14:52:37 +0000

    Fix the releaseend date

--- create.js
+++ create.js
@@ -23,7 +23,7 @@ function buildOptions(ticket, team, component, releaseDate) {
             "customfield_11200": { id: team.id }, // team responsible
             "customfield_15000": ["CheckReleaseStartDateEndDate", "IfRecordClonedPleaseVerifyFields"],
             "customfield_11502": releaseDate,
-            "customfield_11501": moment(releaseDate).add(30, 'minutes').format,
+            "customfield_11501": moment(releaseDate).add(30, 'minutes').format(),
             "priority": { id: "3" }
         }
     })
